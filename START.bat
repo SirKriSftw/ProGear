ddde9623cf3fa109efc2e375ad921ad0c45d3017
Justin U <jamisora-utica@yahoo.com>
2022-05-02 17:50:39 -0400

    API methods for order qty and delete order

--- START.bat
+++ START.bat
@@ -1,4 +1,5 @@
 start C:\Windows\System32\cmd.exe /k dotnet run --project ProGearAPI
 cd ProGear
 start C:\Windows\System32\cmd.exe /k ng serve
+start https://localhost:5001/swagger/index.html
 start http://localhost:4200/home
