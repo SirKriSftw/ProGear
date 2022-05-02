97ae77c087de0edd855e4fdb933e88fc0c8ae8b5
Justin U <jamisora-utica@yahoo.com>
2022-05-02 17:53:39 -0400

    batch file update: now also opens swagger

--- START.bat
+++ START.bat
@@ -1,5 +1,5 @@
 start C:\Windows\System32\cmd.exe /k dotnet run --project ProGearAPI
+start https://localhost:5001/swagger/index.html
 cd ProGear
 start C:\Windows\System32\cmd.exe /k ng serve
-start https://localhost:5001/swagger/index.html
 start http://localhost:4200/home
