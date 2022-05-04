1e395dcc37a0e3bb910df655472340da918f4e05
Jinhwa <jinhwa014@revature.net>
2022-05-04 17:25:44 -0500

    userService Testing update

--- ProGear/src/app/services/userservice/userservice.service.spec.ts
+++ ProGear/src/app/services/userservice/userservice.service.spec.ts
@@ -1,16 +1,90 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserserviceService } from './userservice.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
 
 describe('UserserviceService', () => {
-  let service: UserserviceService;
+  let userservice: UserserviceService;
+  let httpclient : HttpClient; 
+  let httpcontroller : HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserserviceService);
+    TestBed.configureTestingModule({
+      imports : [HttpClientTestingModule],
+      providers : [UserserviceService]
+    });
+    userservice = TestBed.inject(UserserviceService);
+    httpclient = TestBed.inject(HttpClient);
+    httpcontroller = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpcontroller.verify();
+  })
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userservice).toBeDefined();
   });
+
+it('register', () => {
+  const testData = true ; 
+  const inputData = {
+    user : 'admin'
+  }
+
+  userservice.register(inputData.user).subscribe((data : any) => expect(data).toEqual(testData));
+
+  const req = httpcontroller.expectOne('register');
+
+  expect(req.request.method).toEqual('POST');
+
+  req.flush(testData);
+
+})
+
+
+
+  it('getLogin()', () =>{
+    const testData = true ; 
+    const inputData = {
+      email : 'admin',
+      password : 'admin',
+    }   
+
+    userservice.getLogin(inputData.email, inputData.password).subscribe((data : any) => expect(data).toEqual(testData));
+
+    const req = httpcontroller.expectOne('login') ;
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(testData);
+
+  })
+
+
+  it('getLogin() fail', () => {
+    const emsg = 'status 500 error' ;
+    const inputData = {
+      email : 'admin',
+      password : 'admin',
+    };
+
+    userservice.getLogin(inputData.email, inputData.password).subscribe(() => fail('Should have failed with 500 error'),
+    (error : HttpErrorResponse) =>{
+      expect(error.status).toEqual(500, 'status');
+      expect(error.error).toEqual(emsg, 'message');
+    }
+    );
+
+    const req = httpcontroller.expectOne('login');
+    expect(req.request.method).toEqual('GET') ;
+
+    req.flush(emsg, {status : 500, statusText : 'Server Error'});
+
+  });
+
+
+
+
 });
