51c560413b59175fbc9c589ff165d95773f7bbe5
David Acuff <acuffdavid@gmail.com>
2022-05-06 15:41:13 -0400

    service for getting user id

--- ProGear/src/app/services/userservice/userservice.service.ts
+++ ProGear/src/app/services/userservice/userservice.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserserviceService {
-_http:HttpClient
-  constructor(_httpref:HttpClient) { 
-    this._http=_httpref;
+export class UserserviceService implements OnInit{
+
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService, private _http: HttpClient) { 
+    this._http=_http;
   }
+
+  private _userEmail : any;
+  public userId! : number;
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
   register(user:any){
     return this._http.post("https://localhost:44310/api/User/createUser", user,{headers:new HttpHeaders({"Content-Type":"application/json"})});
   }
@@ -18,5 +29,25 @@ _http:HttpClient
     var url = `https://localhost:5001/api/User/Login?email=${email}&password=${password}`;
 
     return this._http.get(url, {responseType: "text"});
+  }
+
+  getUserId(email:any)
+  {
+    //return this._http.get(url, {responseType: "text"});
+    return this._http.get(`https://localhost:5001/api/User/GetId?email=${email}`, {responseType:'text'});
+  }
+
+  getUser()
+  {
+    this.auth.user$.subscribe((data) => {
+      //console.log(data)
+      this._userEmail = data?.email;
+
+      this.getUserId(this._userEmail).subscribe((data) => {
+        //console.log(data);
+        this.userId = parseInt(data);
+      });
+
+    });
   }
 }
