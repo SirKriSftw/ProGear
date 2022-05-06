5b967c3b24d83e30779f415c2021d1fb3d010314
Ahrondus <ahrondus.js@gmail.com>
2022-05-06 16:05:25 -0500

    Updating Sql Database link

--- ProGearAPI/Models/EF/ProGearContext.cs
+++ ProGearAPI/Models/EF/ProGearContext.cs
@@ -29,7 +29,7 @@ namespace ProGearAPI.Models.EF
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
 
-                optionsBuilder.UseSqlServer("server=DESKTOP-5M7ISTP\\JUSTIN_INSTANCE;Initial Catalog=ProGear;Persist Security Info=True;User ID=sa;Password=Password123");
+                optionsBuilder.UseSqlServer("server=p3-progear.database.windows.net; database=progeardb; User ID=project3; Password=Password@123456");
 
             }
         }
