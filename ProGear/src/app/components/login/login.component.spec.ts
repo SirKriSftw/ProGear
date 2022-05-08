219dd0e42b7474bf7eb6d566ec9e80929c180165
Jinhwa <jinhwa014@revature.net>
2022-05-08 17:52:18 -0500

    login and register testing

--- ProGear/src/app/components/login/login.component.spec.ts
+++ ProGear/src/app/components/login/login.component.spec.ts
@@ -1,55 +1,130 @@
 
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { computeMsgId } from '@angular/compiler';
+import { async, ComponentFixture, TestBed, tick , fakeAsync } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { throwError } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
 import { UserserviceService } from 'src/app/services/userservice/userservice.service';
 
 import { LoginComponent } from './login.component'; 
 
 
 describe('LoginComponent', () => {
-  let componentInstance : LoginComponent;
+  let component : LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let el : HTMLElement ; 
-  let loginService : UserserviceService
-//
-  let loginServiceSpy: { login: jasmine.Spy };
-  let routerSpy: { navigateByUrl: jasmine.Spy };
-//
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let userServiceSpyObj : jasmine.SpyObj<UserserviceService> //  1. Make SpyObj 
 
+  beforeEach( async () => {
+    userServiceSpyObj = jasmine.createSpyObj('UserserviceService',['getLogin']) // 2. Initialized the SpyObj
+
+    await TestBed.configureTestingModule({
       imports : [RouterTestingModule, ReactiveFormsModule, FormsModule ],
       declarations: [ LoginComponent ],
-      providers:[HttpClient, UserserviceService, FormBuilder, HttpHandler]
+      providers:[ HttpClient, FormBuilder, HttpHandler,
+      {provide : UserserviceService, useValue : userServiceSpyObj} // 3.  Add to Provide 
+      ]    
     })
     .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
-    componentInstance = fixture.componentInstance ; 
+    component = fixture.componentInstance ; 
   });
 
   beforeEach(() => {
+    userServiceSpyObj.getLogin.and.returnValue(of(""));
     fixture = TestBed.createComponent(LoginComponent);
-    componentInstance = fixture.componentInstance;
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
-    expect(componentInstance).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+
 
   it ('property check', () => {
-    expect(componentInstance.loginForm).toBeDefined();
+    expect(component.loginForm).toBeDefined();
   })
 
-  it ('login', () =>{
-    expect(componentInstance.login('email','password')).toBeDefined()
-  });
-
   
+  it ('verify f', ()=>{
+  
+    expect(component.f).toBe(component.loginForm.controls);
+  })
 
-it('form should be invalid if username is empty', async() => {
+
+
+
+// login testing  -1 
+it('login call success', () =>{
+  //arrange
+  var testData ={
+    email : "email",
+    password : "password",
+    data : "testing" 
+  }
+  userServiceSpyObj.getLogin.and.returnValue(of("testing"));
+  // ACT 
+  component.login(testData.email, testData.password);
+  //Assert 
+  userServiceSpyObj.getLogin(testData.email,testData.password).subscribe((result)=>{
+    expect(result).toEqual(testData.data);
+  // expect(component.getLogin).toEqual(result); // I am not sure this part 
+  })
+})
+
+
+// login testing - 2 
+it ('login call failed', ()=>{
+// Arrange
+  var testData = {
+  email : "email",
+  password : "password",
+  data : new Error('err'),
+}
+let loginResult : any ; 
+userServiceSpyObj.getLogin.and.returnValue(throwError(()=> new Error('err')));
+//Act
+component.login(testData.email, testData.password); 
+// Assert 
+userServiceSpyObj.getLogin(testData.email, testData.password).subscribe(() => {},
+(result) => {
+  loginResult = result;
+});
+expect(loginResult).toEqual(testData.data);
+
+
+})
+
+
+
+})
+
+
+
+
+function data(data: any): never {
+  throw new Error('Function not implemented.');
+}
+/*
+
+it ('login', ()=>{
+  const testData = {
+    email : 'email',
+    password : 'password',
+    result : ''
+  }
+let loginResult : string | undefined; 
+
+ loginServiceSpy.getLogin(testData.email, testData.password ).subscribe((data: string | undefined) => {
+   loginResult = data ; 
+ })
+})
+
+it('form should be invalid if email is empty', async() => {
     componentInstance.loginForm.controls['email'].setValue('');
     componentInstance.loginForm.controls['password'].setValue('password');
     expect(componentInstance.loginForm.valid).toBeFalsy();
@@ -60,6 +135,8 @@ it('form should be invalid if password is empty', async() => {
   componentInstance.loginForm.controls['password'].setValue('');
   expect(componentInstance.loginForm.valid).toBeFalsy();
 });
+
+
 
 it('form should be invalid if password and username both are empty', async() => {
   componentInstance.loginForm.controls['email'].setValue('');
@@ -72,302 +149,8 @@ it('form should be valid if password and username both are not empty', async() =
   componentInstance.loginForm.controls['password'].setValue('password');
   expect(componentInstance.loginForm.valid).toBeTrue();
 });
-  
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { UserserviceService } from 'src/app/services/userservice/userservice.service';
-
-import { LoginComponent } from './login.component'; 
-
-
-describe('LoginComponent', () => {
-  let componentInstance : LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-
-      imports : [RouterTestingModule, ReactiveFormsModule, FormsModule],
-      declarations: [ LoginComponent ],
-      providers:[HttpClient, UserserviceService, FormBuilder, HttpHandler]
-    })
-    .compileComponents();
-    fixture = TestBed.createComponent(LoginComponent);
-    componentInstance = fixture.componentInstance ; 
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    componentInstance = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(componentInstance).toBeTruthy();
-  });
-
-
-  it('email and password are required', () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() =>{
-      const userEmailElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
-      userEmailElement.value ='Jinhwa@revature.net';
-      userEmailElement.dispatchEvent(new Event('input'));
-
-      const userpasswordElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
-      userpasswordElement.value ='12345678';
-      userpasswordElement.dispatchEvent(new Event('input'));
-
-      fixture.detectChanges();
-      fixture.whenStable().then(() =>{
-        const btnElement : HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#button');
-        expect(btnElement.disabled).toBeFalsy();
-        expect(componentInstance.loginForm.get('email')?.value).toEqual('Jinhwa@revature.net');
-        expect(componentInstance.loginForm.get('password')?.value).toEqual('12345678');
-        expect(componentInstance.loginForm.valid).toBeTruthy();
-
-      })
-
-    })
-  })
-
-
-  it('password is in required state', () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() =>{
-      const userEmailElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
-      userEmailElement.value ='Jinhwa@revature.net';
-      userEmailElement.dispatchEvent(new Event('input'));
-
-      const userpasswordElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
-      userpasswordElement.value ='';
-      userpasswordElement.dispatchEvent(new Event('input'));
-
-      fixture.detectChanges();
-      fixture.whenStable().then(() =>{
-        const btnElement : HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#button');
-        expect(btnElement.disabled).toBeTruthy();
-        expect(componentInstance.loginForm.get('email')?.value).toEqual('Jinhwa@revature.net');
-        expect(componentInstance.loginForm.get('password')?.value).toEqual('');
-        expect(componentInstance.loginForm.valid).toBeFalsy();
-
-      })
-
-    })
-  })
-
-
-  it('email is in required state', () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() =>{
-      const userEmailElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
-      userEmailElement.value ='';
-      userEmailElement.dispatchEvent(new Event('input'));
-
-      const userpasswordElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
-      userpasswordElement.value ='12345678';
-      userpasswordElement.dispatchEvent(new Event('input'));
-
-      fixture.detectChanges();
-      fixture.whenStable().then(() =>{
-        const btnElement : HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#button');
-        expect(btnElement.disabled).toBeTruthy();
-        expect(componentInstance.loginForm.get('email')?.value).toEqual('');
-        expect(componentInstance.loginForm.get('password')?.value).toEqual('12345678');
-        expect(componentInstance.loginForm.valid).toBeFalsy();
-
-      })
-
-    })
-  })
-
-  it('check whether ng submit is called or invoked', () =>{
-    fixture.detectChanges();
-    fixture.whenStable().then(() =>{
-  
-      const userEmailElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
-      userEmailElement.value ='Jinhwa@revature.net';
-      userEmailElement.dispatchEvent(new Event('input'));
-  
-      const userpasswordElement : HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
-      userpasswordElement.value ='12345678';
-      userpasswordElement.dispatchEvent(new Event('input'));
-  
-      fixture.autoDetectChanges();
-      fixture.whenStable().then(() =>{
-  
-        const mockFunction = spyOn(componentInstance, 'login').and.callThrough();
-        const buttonElement : HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#button');
-        expect(buttonElement.disabled).toBeFalsy();
-        buttonElement.click();
-        expect(mockFunction).toHaveBeenCalledTimes(1);
-  
-      })
-  
-    })
-  })
-
-
-});
 
 */
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
-import {
-  ComponentFixture,
-  fakeAsync,
-  TestBed,
-  tick,
-  waitForAsync,
-} from '@angular/core/testing';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { UserserviceService } from 'src/app/services/userservice/userservice.service';
-
-import { LoginComponent } from './login.component'; 
-import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
-import { Router } from '@angular/router';
-import { throwError } from 'rxjs';
-
-
-class Page {
-  get submitButton() {
-    return this.fixture.nativeElement.querySelector('#button');
-  }
-  get useremailInput() {
-    return this.fixture.debugElement.nativeElement.querySelector('#email');
-  }
-  get passwordInput() {
-    return this.fixture.debugElement.nativeElement.querySelector('#password');
-  }
-
-  get errorMsg() {
-    return this.fixture.debugElement.query(By.css('.error')).nativeElement;
-  }
-
-  constructor(private fixture: ComponentFixture<LoginComponent>) {}
-
-  public updateValue(input: HTMLInputElement, value: string) {
-    input.value = value;
-    input.dispatchEvent(new Event('input'));
-  }
-}
-
-
-describe('LoginComponent', () => {
-  let componentInstance : LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-  let debugEl: DebugElement;
-
-  let loginService: UserserviceService;
-  let loginServiceSpy: { login: jasmine.Spy };
-  let routerSpy: { navigateByUrl: jasmine.Spy };
-  let router: Router;
-  let page: Page;
-
-
-  beforeEach(() => {
-    loginServiceSpy = jasmine.createSpyObj(UserserviceService, ['login']);
-    routerSpy = jasmine.createSpyObj(Router, ['navigateByUrl']);
-    TestBed.configureTestingModule({
-      imports: [FormsModule],
-      declarations: [LoginComponent],
-      providers: [
-        { provide: UserserviceService, useValue: loginServiceSpy },
-        { provide: Router, useValue: routerSpy }, FormBuilder, FormsModule
-      ],
-    });
-    fixture = TestBed.createComponent(LoginComponent);
-    componentInstance = fixture.componentInstance;
-    debugEl = fixture.debugElement;
-    loginService = TestBed.inject(UserserviceService);
-    router = TestBed.inject(Router);
-    page = new Page(fixture);
-    fixture.detectChanges();
-  });
-
-
-
-  it(
-    'Valid credentials',
-    waitForAsync(() => {
-      page.updateValue(page.useremailInput, '#email');
-      page.updateValue(page.passwordInput, '#password');
-      (loginService.getLogin as jasmine.Spy).and.returnValue(
-        Promise.resolve(true)
-      );
-      page.submitButton.click();
-      fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        const errorArea = debugEl.query(By.css('.error'));
-        expect(errorArea).toBeNull();
-        const navArgs = (router.navigateByUrl as jasmine.Spy).calls.first()
-          .args[0];
-        expect(navArgs).toBe('/home');
-      });
-    })
-  );
-
-
-
-});
-
-
-*/
