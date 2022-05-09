873214248376b45101d59833c24c07145fe69b19
wardtyler96 <101146280+wardtyler96@users.noreply.github.com>
2022-05-09 16:09:31 -0400

    Login Works

--- ProGear/src/app/components/login/login.component.ts
+++ ProGear/src/app/components/login/login.component.ts
@@ -1,26 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '@auth0/auth0-angular';
+import {AuthService, User} from '@auth0/auth0-angular';
+import { HttpClient} from '@angular/common/http';
+import { UserserviceService } from 'src/app/services/userservice/userservice.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(public auth: AuthService) 
+  
+  constructor(public auth: AuthService, public users: UserserviceService) 
   {
    
     }
 
   ngOnInit(): void {
+    this.auth.user$.subscribe((data) => {
+      if (data != null)
+      {
+        this.users.alreadyRegistered(this.users._registerId).subscribe((data2) => { 
+          this.users.ConfirmationMessage = data2;
+          
+          console.log(this.users.ConfirmationMessage);
+        
+         if(this.users.ConfirmationMessage == "User Registered") {
+     
+         }
+         else {
+           console.log("register");
+              this.users.register(data.sub, data.email, data.given_name, data.family_name).subscribe((data3) => 
+              {});
+         }})
+        // sub is the user_id
+        console.log(data.sub);
+        
+      }
+    })
+
+  
   }
 
   loginUserWithRedirect()
   {
-    console.log("User should be below me.");
     this.auth.loginWithRedirect();
-    console.log(this.auth.user$);
     
+  
   }
+ 
 
 }
--- ProGear/src/app/services/userservice/userservice.service.ts
+++ ProGear/src/app/services/userservice/userservice.service.ts
@@ -1,46 +1,45 @@
-<<<<<<< HEAD
-import { Component, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
-import {AuthService} from '@auth0/auth0-angular';
-=======
 import { Injectable } from '@angular/core';
 import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
->>>>>>> origin/Login-Login
 
 @Injectable({
   providedIn: 'root'
 })
-<<<<<<< HEAD
-export class UserserviceService {
-_http:HttpClient
-  constructor(_httpref:HttpClient, public auth: AuthService) { 
-    this._http=_httpref;
-=======
 export class UserserviceService implements OnInit{
 
   constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService, private _http: HttpClient) { 
     this._http=_http;
->>>>>>> origin/Login-Login
+  
+
   }
 
-  private _userEmail : any;
+  public _userEmail : any;
   public userId! : number;
+  public _registerId?:string = "";
+  public _firstName?:string = "";
+  public _lastName?:string = "";
+  public ConfirmationMessage?:string = "";
+  
 
   ngOnInit(): void {
     this.getUser();
   }
 
-  register(user:any){
-    //var name = this.auth.user$.name;
-    return this._http.post("https://localhost:44310/api/User/createUser", user,{headers:new HttpHeaders({"Content-Type":"application/json"})});
+  register(_registerId?:string,_userEmail?:string,_firstName?:string,_lastName?:string){
+    //var name = this.auth.user$.name; 
+    var data = {
+      "UserId":_registerId,
+      "Email":_userEmail,
+      "FirstName":_firstName,
+      "LastName":_lastName,
+    }
+    console.log(data);
+    return this._http.post(`https://localhost:44310/api/User/Register`,data, {headers:new HttpHeaders({"Content-Type":"application/json"})});
   }
-}
 
-<<<<<<< HEAD
-=======
+
   getLogin(email : string, password : string)
   {
     var url = `https://localhost:5001/api/User/Login?email=${email}&password=${password}`;
@@ -54,18 +53,29 @@ export class UserserviceService implements OnInit{
     return this._http.get(`https://localhost:5001/api/User/GetId?email=${email}`, {responseType:'text'});
   }
 
+  alreadyRegistered(_registerId?: string) {
+    return this._http.get("https://localhost:44310/api/User/CheckForUser?userid=" + _registerId, {responseType: 'text'});
+  }
+  
   getUser()
   {
+    console.log("Test");
     this.auth.user$.subscribe((data) => {
       //console.log(data)
+      console.log("Test");
       this._userEmail = data?.email;
-
-      this.getUserId(this._userEmail).subscribe((data) => {
-        //console.log(data);
-        this.userId = parseInt(data);
-      });
+      this._registerId = data?.sub;
+      this._firstName = data?.given_name;
+      this._lastName = data?.family_name;
+      console.log(this._registerId);
+      console.log(this._firstName);
+      console.log(this._lastName);
+      // this.getUserId(this._userEmail).subscribe((data) => {
+      //   //console.log(data);
+      //   this.userId = parseInt(data);
+      // });
 
     });
   }
+  
 }
->>>>>>> origin/Login-Login
