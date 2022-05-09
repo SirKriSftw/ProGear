df0a63a9b3c2289b3c44c738e733d4d415e39588
Kristian Perez <kpere192@fiu.edu>
2022-05-09 18:45:56 -0400

    Fixed small routing issue when pressing VIEW ALL in home page

--- ProGear/src/app/app-routing.module.ts
+++ ProGear/src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
 import { ProductsComponent } from './components/products/products.component';
-// import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-{path:'', component:HomeComponent},
+{path:'', redirectTo: 'home', pathMatch:'full'},
 {path:'home', component:HomeComponent},
 {path:'products',component:ProductsComponent},
-{path:'login', component:LoginComponent},
+{path:'login', redirectTo: 'home', pathMatch:'full'},
 {path:'cart', component:CartComponent}
 ];
 
