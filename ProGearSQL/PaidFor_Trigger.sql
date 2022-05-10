0075c8cf4d8f15a92a1bf949119aad7c11f4cd9e
Kristian Perez <kpere192@fiu.edu>
2022-05-10 19:28:08 -0400

    Update to Paid For trigger

--- ProGearSQL/PaidFor_Trigger.sql
+++ ProGearSQL/PaidFor_Trigger.sql
@@ -1,10 +1,8 @@
-/****** Object:  Trigger [dbo].[changeQTY]    Script Date: 5/4/2022 12:21:01 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TRIGGER paidFor_trg 
-on Cart
+alter trigger paidFor_trg on Cart
 after update 
 as 
 declare @paidfor bit;
@@ -26,6 +24,10 @@ declare @Prod_ID int;
 declare @cart_ID int;
 declare @order_ID int;
 declare @pass int;
+declare @userid varchar(100);
+
+
+
 set @pass =0;
 declare @error int;
 set @error = 0;
@@ -37,8 +39,13 @@ select @cart_ID =list.cartID from inserted list;
 --print 'this is cart id';
 print @cart_ID;
 
-
-
+set @userid = (select users.userID from Users 
+full outer join Cart 
+on  Users.userID = cart.userID 
+where cartID = @cart_ID
+)
+print 'user id'
+print @userid;
 
 declare MultiRow cursor for 
 select Products.Productid from Products
@@ -134,14 +141,11 @@ begin
 						print 'product stock has been updated';
 						
 						
-						
-						
-						
-						
 						fetch next from MultiRow2  into @Prod_ID
 						
 
 					end
+					insert Cart values (@userid , null,0,null)
 				close MultiRow2
 				DEALLOCATE MultiRow2
 			end
